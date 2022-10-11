23e19c0cc0e66170254a7b1c4723ff426862254a
LuyolG <1402098743@qq.com>
2022-10-11 16:42:40 +0800

    tsts

--- p17.ts
+++ p17.ts
@@ -9,5 +9,16 @@
 // console.log(props(o,"b"));
 // console.log(props(123,"toString"));
 
-
 // --------------------------------------------------------------
+class Sub<T> {
+  attr: T[] = [];
+  add(a: T): T[] {
+    return [a];
+  }
+}
+
+let s = new Sub<number>();
+console.log((s.attr = [1]), s.add(2));
+
+let str = new Sub<string>();
+console.log(str.attr = ['1'],str.add('123'));
